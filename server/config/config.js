33f064ae4d6ab9fe02a8624fd8a181da47666d78
Gustavo <Gustavomarianodelgado@gmail.com>
2018-06-16 22:29:56 -0300

    variable de entorno Mongo

--- server/config/config.js
+++ server/config/config.js
@@ -16,7 +16,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb://cafegustavo:a123456@ds163330.mlab.com:63330/cafe_gustavo';
+    urlDB = process.env.MONGO_URI;
 }
 
 
