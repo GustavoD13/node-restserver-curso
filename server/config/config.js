a3ff896fd8c5a6bcf8f64c40ed28bf5cb8ecfa55
Gustavo <Gustavomarianodelgado@gmail.com>
2018-07-08 00:40:38 -0300

    Login Normal seccion 10

--- server/config/config.js
+++ server/config/config.js
@@ -7,10 +7,24 @@ process.env.PORT = process.env.PORT || 3000;
 //==================================
 process.env.NODE_ENV = process.env.NODE_ENV || 'dev'
 
+//==================================
+// Vencimiento del Token
+//==================================
+//60 segundos
+//60 minutos
+//24 horas
+//30 días
+
+process.env.CADUCIDAD_TOKEN = 60 * 60 * 24 * 30
+
 
 //==================================
-// Base de datos
+// SEED de autenticacion
 //==================================
+process.env.SEED = process.env.SEED || 'este-es-el-seed-desarrollo'
+    //==================================
+    // Base de datos
+    //==================================
 let urlDB;
 
 if (process.env.NODE_ENV === 'dev') {
--- server/routes/usuario.js
+++ server/routes/usuario.js
@@ -3,10 +3,13 @@ const Usuario = require('../models/usuario');
 const app = express();
 const bcrypt = require('bcrypt');
 
+const { verificaToken, verificaAdmin_Role } = require('../middlewares/autenticacion');
+
 const _ = require('underscore');
 
 
-app.get('/usuario', function(req, res) {
+app.get('/usuario', verificaToken, function(req, res) {
+
 
 
     let desde = req.query.desde || 0;
@@ -41,7 +44,7 @@ app.get('/usuario', function(req, res) {
 
 });
 
-app.post('/usuario', function(req, res) {
+app.post('/usuario', [verificaToken, verificaAdmin_Role], function(req, res) {
 
     let body = req.body;
     let usuario = new Usuario({
@@ -74,7 +77,7 @@ app.post('/usuario', function(req, res) {
 });
 
 
-app.put('/usuario/:id', function(req, res) {
+app.put('/usuario/:id', [verificaToken, verificaAdmin_Role], function(req, res) {
 
     let id = req.params.id;
     let body = _.pick(req.body, ['nombre', 'email', 'img', 'role', 'estado']);
@@ -103,7 +106,7 @@ app.put('/usuario/:id', function(req, res) {
 
 });
 
-app.delete('/usuario/:id', function(req, res) {
+app.delete('/usuario/:id', [verificaToken, verificaAdmin_Role], function(req, res) {
 
     let id = req.params.id;
     let cambiaestado = {
